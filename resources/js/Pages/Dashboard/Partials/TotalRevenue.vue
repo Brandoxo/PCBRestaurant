b31012f57df09d66595971ad3f17be17c99c8205
Brandoxo <brandoxo90@gmail.com>
2025-08-25 15:24:10 -0600

    UPDATE\ color badge

--- resources/js/Pages/Dashboard/Partials/TotalRevenue.vue
+++ resources/js/Pages/Dashboard/Partials/TotalRevenue.vue
@@ -5,7 +5,7 @@
 <template>
     <div class="w-full ">
         <div class="bg-white rounded-xl shadow p-6 flex gap-4 justify-center items-center">
-            <div class="rounded-full bg-orangeYellow/30 flex items-center justify-center w-14 h-14">
+            <div class="rounded-full bg-goodYellow/30 flex items-center justify-center w-14 h-14">
                 <img src="/assets/icons/svg/dash/revenue.svg" alt="Total Revenue" class="h-10 w-10" />
             </div>
             <div class="">
