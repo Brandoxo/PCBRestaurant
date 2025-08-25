801e7eb1f0b5660c1332c82ca39e1ba722c6073d
Brandoxo <brandoxo90@gmail.com>
2025-08-25 14:14:04 -0600

    ADD\ dashboard totals div

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -2,6 +2,9 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head } from "@inertiajs/vue3";
 import Header from "@/Components/Header.vue";
+import TotalOrders from "@/Pages/Dashboard/Partials/TotalOrders.vue";
+import TotalRevenue from "@/Pages/Dashboard/Partials/TotalRevenue.vue";
+import TotalCancelled from "@/Pages/Dashboard/Partials/TotalCancelled.vue";
 </script>
 
 <template>
@@ -10,12 +13,18 @@ import Header from "@/Components/Header.vue";
     <AuthenticatedLayout>
         <Header></Header>
 
-        <div class="py-12">
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8 ml-64">
+        <div class="py-12 ">
+            <div class="mx-auto sm:px-6 lg:px-8 ml-64">
                 <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
                         ¡Bienvenido al Panel de Control!
                     </div>
+                </div>
+
+                <div class="md:flex justify-between gap-2 md:gap-8 lg:gap-14  2xl:gap-60 mt-6 ">
+                    <TotalOrders :totalOrders="totalOrders" />
+                    <TotalRevenue :totalRevenue="totalRevenue" />
+                    <TotalCancelled :totalCancelled="totalCancelled" />
                 </div>
             </div>
         </div>
---
+++ resources/js/Pages/Dashboard/Partials/TotalRevenue.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="w-full ">
+        <div class="bg-white rounded-xl shadow p-6 flex gap-4 justify-center items-center">
+            <div class="rounded-full bg-orangeYellow/30 flex items-center justify-center w-14 h-14">
+                <img src="/assets/icons/svg/dash/revenue.svg" alt="Total Revenue" class="h-10 w-10" />
+            </div>
+            <div class="">
+                <h2 class="text-xl font-medium">Ingresos Totales</h2>
+                <p class="text-3xl font-bold text-gray-800"><span>$ </span>{{ totalRevenue ?? 0 }} </p>
+            </div>
+        </div>
+    </div>
+</template>
