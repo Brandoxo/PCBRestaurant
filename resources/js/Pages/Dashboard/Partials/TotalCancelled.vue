ab635ad02d3e18729826e1441a73b0eecc2ac4a0
Brandon Bermejo <brandoxo90@gmail.com>
2025-08-25 19:14:42 -0600

    UPDATE\ radius totals

--- resources/js/Pages/Dashboard/Partials/TotalCancelled.vue
+++ resources/js/Pages/Dashboard/Partials/TotalCancelled.vue
@@ -1,16 +1,24 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
     <div class="w-full">
-        <div class="bg-white rounded-xl shadow p-6 flex gap-4 justify-center items-center">
-            <div class="rounded-full bg-dangerRed/30 flex items-center justify-center w-14 h-14">
-                <img src="/assets/icons/svg/dash/cancelled.svg" alt="Total Canceladas" class="h-10 w-10" />
+        <div
+            class="bg-white rounded-3xl shadow p-6 flex gap-4 justify-center items-center"
+        >
+            <div
+                class="rounded-full bg-dangerRed/30 flex items-center justify-center w-14 h-14"
+            >
+                <img
+                    src="/assets/icons/svg/dash/cancelled.svg"
+                    alt="Total Canceladas"
+                    class="h-10 w-10"
+                />
             </div>
             <div class="">
                 <h2 class="text-xl font-medium">Total Canceladas</h2>
-                <p class="text-3xl font-bold text-gray-800">{{ totalCancelled ?? 0 }} </p>
+                <p class="text-3xl font-bold text-gray-800">
+                    {{ totalCancelled ?? 0 }}
+                </p>
             </div>
         </div>
     </div>
--- resources/js/Pages/Dashboard/Partials/TotalOrders.vue
+++ resources/js/Pages/Dashboard/Partials/TotalOrders.vue
@@ -1,16 +1,24 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-    <div class="w-full ">
-        <div class="bg-white rounded-xl shadow p-6 flex gap-4 justify-center items-center">
-            <div class="rounded-full bg-approveGreen/30 flex items-center justify-center w-14 h-14">
-                <img src="/assets/icons/svg/dash/cube.svg" alt="Total Orders" class="h-10 w-10" />
+    <div class="w-full">
+        <div
+            class="bg-white rounded-3xl shadow p-6 flex gap-4 justify-center items-center"
+        >
+            <div
+                class="rounded-full bg-approveGreen/30 flex items-center justify-center w-14 h-14"
+            >
+                <img
+                    src="/assets/icons/svg/dash/cube.svg"
+                    alt="Total Orders"
+                    class="h-10 w-10"
+                />
             </div>
             <div class="">
                 <h2 class="text-xl font-medium">Ordenes Totales</h2>
-                <p class="text-3xl font-bold text-gray-800">{{ totalOrders ?? 0 }} </p>
+                <p class="text-3xl font-bold text-gray-800">
+                    {{ totalOrders ?? 0 }}
+                </p>
             </div>
         </div>
     </div>
