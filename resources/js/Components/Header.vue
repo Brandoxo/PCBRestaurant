2babc499b912543488d16a9d366ad3c5b1ac5b71
Brandoxo <brandoxo90@gmail.com>
2025-09-02 13:26:17 -0600

    WO\ menu responsive

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -28,7 +28,26 @@ const title = computed(() => {
         <div
             class="flex justify-between py-[15px] px-4 sm:px-6 lg:px-8 gap-4 lg:mr-64 "
         >
-            <h1 class="text-2xl font-bold text-gray-900 hidden lg:block">{{ title }}</h1>
+            <!--hamburger menu-->
+            <div class="md:hidden flex items-center">
+                <button @click="">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M4 6h16M4 12h16m-7 6h7"
+                        />
+                    </svg>
+                </button>
+            </div>
+            <h1 class="text-2xl font-bold text-gray-900 hidden md:block">{{ title }}</h1>
             <div class="flex gap-4 items-center justify-center">
                 <form
                     v-if="title === 'Dashboard' || title === 'Órdenes'"
