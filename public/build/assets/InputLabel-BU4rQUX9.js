2f02cf2ba21bd4aff591a4b8bf1e2c21f6650e4e
Brandoxo <brandoxo90@gmail.com>
2025-10-09 12:49:53 -0600

    BUILD\ 091025 1249

---
+++ public/build/assets/InputLabel-BU4rQUX9.js
@@ -0,0 +1,1 @@
+import{g as t,o as e,t as o,r as l}from"./app-BsmVB6Bw.js";const n={class:"block text-sm font-bold text-black"},c={key:0},r={key:1},i={__name:"InputLabel",props:{value:{type:String}},setup(s){return(a,_)=>(e(),t("label",n,[s.value?(e(),t("span",c,o(s.value),1)):(e(),t("span",r,[l(a.$slots,"default")]))]))}};export{i as _};
