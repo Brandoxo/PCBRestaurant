a5802f83f2d68534811e63df4f2850a4510bb66c
Brandoxo <brandoxo90@gmail.com>
2025-10-29 16:38:00 -0600

    WO\ Room Service feature

--- resources/js/Pages/Config/Index.vue
+++ resources/js/Pages/Config/Index.vue
@@ -3,8 +3,8 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import ShiftSchedules from './Partials/ShiftSchedules.vue';
 import Discounts from './Partials/Discounts.vue';
 import CashFloat from './Partials/CashFloat.vue';
-import { usePage } from '@inertiajs/vue3';
-import { computed } from 'vue';
+import RoomService from './Partials/RoomService.vue';
+
 
 const props = defineProps({
     shifts: Array,
@@ -22,12 +22,13 @@ const props = defineProps({
             </h2>
         </template>
 
-        <div class="py-4">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 h-screen overflow-y-auto py-60 pt-0">
+        <div class="py-4  h-screen overflow-y-auto">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-60 pt-0">
                 <div class=" overflow-hidden shadow-sm sm:rounded-lg gap-6 flex flex-col">
                     <CashFloat :cashFloats="props.cashFloats" />
                     <ShiftSchedules :shifts="props.shifts"/>
                     <Discounts />
+                    <RoomService />
                 </div>
             </div>
         </div>
---
+++ resources/js/Pages/Config/Partials/RoomService.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue';
+import Switch from '@/Components/Switch.vue';
+
+const isRoomServiceEnabled = ref(false);
+const roomServiceCharge = ref(0);
+</script>
+<template>
+    <div class="bg-white shadow-md rounded-b p-6">
+        <h3 class="text-lg font-semibold">Room Service</h3>
+        <p class="mb-6 text-sm text-gray-600">Configura las opciones de Room Service en el sistema.</p>
+
+        <div class="mb-4">
+            <label class="block text-sm font-medium text-gray-700">Habilitar Room Service
+
+            <input
+                type="checkbox"
+                class="sr-only peer relative"
+                v-model="isRoomServiceEnabled"
+                id="is_active"
+            />
+
+            <Switch v-model="isRoomServiceEnabled" />
+            </label>
+
+        </div>
+
+        <div v-if="isRoomServiceEnabled">
+            <label class="block text-sm font-medium text-gray-700">Cargo Adicional por Room Service (%)</label>
+            <input
+                type="number"
+                min="0"
+                max="100"
+                class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+                v-model.number="roomServiceCharge"
+            />
+        </div>
+    </div>
+</template>
