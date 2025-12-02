eaa3537ef0230567776386c94a2174a9d6d200b2
Brandoxo <brandoxo90@gmail.com>
2025-12-02 16:10:29 -0600

    UPDATE\ Notes Component Size

--- resources/js/Pages/Orders/Partials/Notes.vue
+++ resources/js/Pages/Orders/Partials/Notes.vue
@@ -28,9 +28,9 @@ watch(localNotes, (val) => {
         <transition
             enter-active-class="transition-all duration-300"
             enter-from-class="opacity-0 max-h-0"
-            enter-to-class="opacity-100 max-h-40"
+            enter-to-class="opacity-100 max-h-20"
             leave-active-class="transition-all duration-200"
-            leave-from-class="opacity-100 max-h-40"
+            leave-from-class="opacity-100 max-h-20"
             leave-to-class="opacity-0 max-h-0"
         >
             <div v-if="openInput" class="mt-2 overflow-hidden">
@@ -38,7 +38,7 @@ watch(localNotes, (val) => {
                     <li>
                         <textarea
                             v-model="localNotes"
-                            class="w-64 h-32 p-2 border border-gray-300 rounded resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            class="w-64 h-20 p-2 border border-gray-300 rounded resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"
                             placeholder="Ej: Sin sal, extra picante, etc."
                         ></textarea>
                     </li>
