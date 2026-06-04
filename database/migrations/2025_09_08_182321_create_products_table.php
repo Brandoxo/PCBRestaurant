8c3bcda9ba76d937b81387bc3a76729edc5074ef
Brandoxo <brandoxo90@gmail.com>
2026-06-04 13:43:48 -0600

    ADD\ SoftDeletes values

--- database/migrations/2025_09_08_182321_create_products_table.php
+++ database/migrations/2025_09_08_182321_create_products_table.php
@@ -22,7 +22,6 @@ return new class extends Migration
             $table->decimal('cost', 12)->default(0);
             $table->decimal('tax_rate', 5)->default(0);
             $table->boolean('is_active')->default(true);
-            $table->softDeletes();
             $table->timestamps();
         });
     }
